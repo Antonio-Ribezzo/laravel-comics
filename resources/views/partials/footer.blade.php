0b3dd5d85f6b049b1ba9984e4ea402ee742a0f7d
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-08 16:52:26 +0200

    Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,20 @@
 <footer>
-    <h2 class="text-danger">footer</h2>
+     <footer>
+        <div class="container py-4 d-flex justify-content-between align-items-center">
+            <!-- footer sx -->
+            <div>
+                <button class="text-uppercase text-white px-3 py-2">sign-up now!</button>
+            </div>
+            <!-- footer dx -->
+            <div class="d-flex justify-content-between align-items-center">
+                <span class="text-uppercase colorLightBlue mx-3">follow us</span>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook-logo" class="mx-2"></a>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter-logo" class="mx-2"></a>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube-logo" class="mx-2"></a>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest-logo" class="mx-2"></a>
+                <a href="#"><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope-logo" class="mx-2"></a>
+            </div>
+        </div>
+    </footer>
 
 </footer>
