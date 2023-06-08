d05d8ca9d9162dc112b0b8c414eb2c478d0d03bb
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-08 17:58:56 +0200

    Foreach (card Items) - Finish

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,29 @@
 
 @section('contentHome') 
 
+    <section id="mainSection">
+        <div class="container py-5 text-center position-relative">
+            <div>
+                <span id="currentSeries" class="text-uppercase text-white position-absolute">current series</span>
+            </div>
+            <ul class="d-flex justify-content-between align-items-start flex-wrap">
+                <!-- card -->
+                @foreach ($arrayComics as $el)
+                    <li class="my-4">
+                        <a class="text-white" href="#">
+                            <img src="{{$el['thumb']}}" alt="{{$el['series']}}">
+                            <p class="mt-2 text-start">{{$el['series']}}</p>
+                            <span class="m-0 text-start">{{$el['price']}}</span>
+                            <span class="m-0 text-start">{{$el['type']}}</span>
+                        </a>
+                    </li> 
+                @endforeach
+            </ul>
+            <button class="text-white text-uppercase">load more</button>
+        </div>
 
+
+    </section>
 
 
 
